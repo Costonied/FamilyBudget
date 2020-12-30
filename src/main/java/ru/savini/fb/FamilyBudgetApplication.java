fe6bf9360b04879f2e164926f52f284150414322
SavinI <Savin.I.Vladi@sberbank.ru>
2020-12-30 07:46:17 +0300

    Init commit

---
+++ src/main/java/ru/savini/fb/FamilyBudgetApplication.java
@@ -0,0 +1,54 @@
+package ru.savini.fb;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+//import ru.savini.fb.domain.Account;
+//import ru.savini.fb.repo.AccountRepository;
+
+@SpringBootApplication
+//@PropertySource("classpath:application.properties")
+public class FamilyBudgetApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(FamilyBudgetApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilyBudgetApplication.class);
+    }
+
+//    @Bean
+//    public CommandLineRunner loadData(AccountRepository repository) {
+//        return (args) -> {
+//            // save a couple of customers
+//            repository.save(new Account("Наличные", 10.0));
+//            repository.save(new Account("Карта Сбербанк", 2.34));
+//
+//            // fetch all customers
+//            LOGGER.info("Accounts found with findAll():");
+//            LOGGER.info("-------------------------------");
+//            for (Account account : repository.findAll()) {
+//                LOGGER.info(account.toString());
+//            }
+//            LOGGER.info("");
+//
+//            // fetch an individual account by ID
+//            Account account = repository.findById(1L).get();
+//            LOGGER.info("Account found with findOne(1L):");
+//            LOGGER.info("--------------------------------");
+//            LOGGER.info(account.toString());
+//            LOGGER.info("");
+//
+//            // fetch accounts by last name
+//            LOGGER.info("Account found with findByNameStartsWithIgnoreCase('Карта'):");
+//            LOGGER.info("--------------------------------------------");
+//            for (Account card : repository
+//                    .findByNameStartsWithIgnoreCase("Карта")) {
+//                LOGGER.info(card.toString());
+//            }
+//            LOGGER.info("");
+//        };
+//    }
+}
