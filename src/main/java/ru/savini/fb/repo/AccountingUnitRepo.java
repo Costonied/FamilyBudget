a93bc266b269fb08f58ec70d83346e214a2303ee
Savin Igor <Savin.I.Vladi@sberbank.ru>
2021-03-02 22:44:47 +0300

    Corrected getting AccountingUnit from repo
    * added field "year" in the filter

--- src/main/java/ru/savini/fb/repo/AccountingUnitRepo.java
+++ src/main/java/ru/savini/fb/repo/AccountingUnitRepo.java
@@ -6,5 +6,5 @@ import ru.savini.fb.domain.entity.Category;
 import ru.savini.fb.domain.entity.AccountingUnit;
 
 public interface AccountingUnitRepo extends JpaRepository<AccountingUnit, Long> {
-    AccountingUnit findByCategoryAndMonth(Category category, int month);
+    AccountingUnit findByCategoryAndYearAndMonth(Category category, int year, int month);
 }
