b4f6b565339895eb6061d1e34b333b820b45dc2a
Savin Igor <Savin.I.Vladi@sberbank.ru>
2021-03-03 22:57:54 +0300

    Switch GSheets service to work with real budget sheet
    * Also deleted class GSheetsUtils because not needed more

--- src/main/java/ru/savini/fb/gsheets/GSheetsInfo.java
+++ src/main/java/ru/savini/fb/gsheets/GSheetsInfo.java
@@ -5,9 +5,9 @@ package ru.savini.fb.gsheets;
  */
 public class GSheetsInfo {
     /* Common info about document */
-    public static final String SPREADSHEET_ID = "1BB-MFczIhtcZdYPUS9htPEpMsu-vxCDaFcQ4qsjWqgY";
+    public static final String SPREADSHEET_ID = "1tiFfn04mqxPkJHKfAw9pRK8v9UeHcgUGtJhvYaRld20";
     /* Transactions info */
-    public static final String TRANSACTIONS_RANGE = "Transactions!A2:F";
+    public static final String TRANSACTIONS_RANGE = "2021-T!A2:F";
 
     private GSheetsInfo() {
         // just hide constructor
