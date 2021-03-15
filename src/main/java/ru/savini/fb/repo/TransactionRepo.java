cdd888f0e8c129146a224a93def06b8733211422
Savin Igor <Savin.I.Vladi@sberbank.ru>
2021-03-15 21:08:18 +0300

    Deleted not needed import in TransactionRepo

--- src/main/java/ru/savini/fb/repo/TransactionRepo.java
+++ src/main/java/ru/savini/fb/repo/TransactionRepo.java
@@ -3,8 +3,6 @@ package ru.savini.fb.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.savini.fb.domain.entity.Transaction;
 
-import java.util.List;
-
 public interface TransactionRepo extends JpaRepository<Transaction, Long> {
 
 }
