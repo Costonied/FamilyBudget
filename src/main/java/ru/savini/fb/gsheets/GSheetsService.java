a7f52db84b489ef78fcac9015eb647f71326bea8
Igor Savin <costonied@gmail.com>
2021-08-06 22:12:41 +0300

    Delete Google Sheets from project because it's stop me

--- src/main/java/ru/savini/fb/gsheets/GSheetsService.java
+++
@@ -1,9 +0,0 @@
-package ru.savini.fb.gsheets;
-
-import ru.savini.fb.domain.entity.Transaction;
-
-import java.io.IOException;
-
-public interface GSheetsService {
-    void addTransaction(Transaction transaction) throws IOException;
-}
