5a2b57467f656d9fbe2a961108fe6e04d939221d
Igor Savin <costonied@gmail.com>
2021-09-10 22:30:59 +0300

    Application settings feature added

---
+++ src/main/java/ru/savini/fb/controller/AppSettingsController.java
@@ -0,0 +1,13 @@
+package ru.savini.fb.controller;
+
+import ru.savini.fb.domain.entity.settings.AppSettings;
+
+import java.util.List;
+
+public interface AppSettingsController {
+    void save(AppSettings category);
+    void deleteById(Long id);
+
+    List<AppSettings> getAll();
+    List<AppSettings> getByKeyStartsWithIgnoreCase(String name);
+}
---
+++ src/main/java/ru/savini/fb/controller/AppSettingsControllerImpl.java
@@ -0,0 +1,39 @@
+package ru.savini.fb.controller;
+
+import java.util.List;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import ru.savini.fb.domain.entity.settings.AppSettings;
+import ru.savini.fb.repo.AppSettingsRepo;
+
+
+@Component
+public class AppSettingsControllerImpl implements AppSettingsController {
+    private final AppSettingsRepo repo;
+
+    @Autowired
+    public AppSettingsControllerImpl(AppSettingsRepo appSettingsRepo) {
+        this.repo = appSettingsRepo;
+    }
+
+    @Override
+    public void save(AppSettings category) {
+        repo.save(category);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        repo.deleteById(id);
+    }
+
+    @Override
+    public List<AppSettings> getAll() {
+        return repo.findAll();
+    }
+
+    @Override
+    public List<AppSettings> getByKeyStartsWithIgnoreCase(String key) {
+        return repo.findByKeyStartsWithIgnoreCase(key);
+    }
+}
