833886b33542267de1efb3b9670f81a7cc8fe6b6
Igor Savin <costonied@gmail.com>
2021-10-31 01:43:22 +0300

    Main layout redesign
    * More clear layout
    * Logout button in navbar

--- src/main/java/ru/savini/fb/ui/views/MainView.java
+++ src/main/java/ru/savini/fb/ui/views/MainView.java
@@ -5,45 +5,38 @@ import com.vaadin.flow.component.ComponentUtil;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.html.Anchor;
-import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
-import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.component.tabs.TabsVariant;
-import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
 import com.vaadin.flow.router.RouterLink;
+import ru.savini.fb.globals.Color;
 
 import java.util.Optional;
 
 @Route("main")
 @RouteAlias(value = "")
 public class MainView extends AppLayout {
-
     private final Tabs menu;
-    private H1 viewTitle;
+    private Anchor logout;
 
     public MainView() {
-        setPrimarySection(Section.DRAWER);
-        addToNavbar(true, createHeaderContent());
+        initLogout();
+        DrawerToggle toggle = new DrawerToggle();
+        addToNavbar(true, toggle, logout);
         menu = createMenu();
         addToDrawer(createDrawerContent(menu));
     }
 
-    private Component createHeaderContent() {
-        HorizontalLayout layout = new HorizontalLayout();
-        layout.setId("header");
-        layout.getThemeList().set("dark", true);
-        layout.setWidthFull();
-        layout.setSpacing(false);
-        layout.setAlignItems(FlexComponent.Alignment.CENTER);
-        layout.add(new DrawerToggle());
-        viewTitle = new H1();
-        layout.add(viewTitle);
-        return layout;
+    private void initLogout() {
+        Icon icon = VaadinIcon.SIGN_OUT.create();
+        icon.setColor(Color.NATIVE_BLUE);
+        logout = new Anchor("logout", icon);
     }
 
     private Component createDrawerContent(Tabs menu) {
@@ -53,12 +46,7 @@ public class MainView extends AppLayout {
         layout.setSpacing(false);
         layout.getThemeList().set("spacing-s", true);
         layout.setAlignItems(FlexComponent.Alignment.STRETCH);
-        HorizontalLayout logoLayout = new HorizontalLayout();
-        logoLayout.setId("logo");
-        logoLayout.setAlignItems(FlexComponent.Alignment.CENTER);
-        logoLayout.add(new H1("FB"));
-        Anchor logout = new Anchor("logout", "Log out");
-        layout.add(logoLayout, menu, logout);
+        layout.add(menu);
         return layout;
     }
 
@@ -91,15 +79,10 @@ public class MainView extends AppLayout {
     protected void afterNavigation() {
         super.afterNavigation();
         getTabForComponent(getContent()).ifPresent(menu::setSelectedTab);
-        viewTitle.setText(getCurrentPageTitle());
     }
 
     private Optional<Tab> getTabForComponent(Component component) {
         return menu.getChildren().filter(tab -> ComponentUtil.getData(tab, Class.class).equals(component.getClass()))
                 .findFirst().map(Tab.class::cast);
-    }
-
-    private String getCurrentPageTitle() {
-        return getContent().getClass().getAnnotation(PageTitle.class).value();
     }
 }
