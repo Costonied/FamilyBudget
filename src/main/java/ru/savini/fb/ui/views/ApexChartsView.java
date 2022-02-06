188b78850a989586fa0f6b60205bd7d20b63f8c1
u16712687 <Savin.I.Vladi@sberbank.ru>
2022-02-06 08:46:24 +0300

    ApexCharts circle diagram added

--- src/main/java/ru/savini/fb/ui/views/ApexChartsView.java
+++ src/main/java/ru/savini/fb/ui/views/ApexChartsView.java
@@ -3,25 +3,18 @@ package ru.savini.fb.ui.views;
 import com.github.appreciated.apexcharts.ApexCharts;
 import com.github.appreciated.apexcharts.ApexChartsBuilder;
 import com.github.appreciated.apexcharts.config.builder.ChartBuilder;
-import com.github.appreciated.apexcharts.config.builder.DataLabelsBuilder;
 import com.github.appreciated.apexcharts.config.builder.LegendBuilder;
-import com.github.appreciated.apexcharts.config.builder.StrokeBuilder;
-import com.github.appreciated.apexcharts.config.builder.TitleSubtitleBuilder;
-import com.github.appreciated.apexcharts.config.builder.XAxisBuilder;
-import com.github.appreciated.apexcharts.config.builder.YAxisBuilder;
+import com.github.appreciated.apexcharts.config.builder.ResponsiveBuilder;
 import com.github.appreciated.apexcharts.config.chart.Type;
-import com.github.appreciated.apexcharts.config.chart.builder.ZoomBuilder;
-import com.github.appreciated.apexcharts.config.legend.HorizontalAlign;
-import com.github.appreciated.apexcharts.config.stroke.Curve;
-import com.github.appreciated.apexcharts.config.subtitle.Align;
-import com.github.appreciated.apexcharts.config.xaxis.XAxisType;
+import com.github.appreciated.apexcharts.config.legend.Position;
+import com.github.appreciated.apexcharts.config.responsive.builder.OptionsBuilder;
 import com.github.appreciated.apexcharts.helper.Series;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
-import java.time.LocalDate;
-import java.util.stream.IntStream;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Route(value = "apexcharts", layout = MainView.class)
@@ -34,30 +27,26 @@ public class ApexChartsView extends VerticalLayout {
     }
 
     private ApexCharts getApexCharts() {
+        List<Series<Double>> series = new ArrayList<>();
+        series.add(new Series<>("Образование", 11.0));
+        series.add(new Series<>("Коты", 2.0));
+        series.add(new Series<>("Домашняя еда", 35.0));
+
         return ApexChartsBuilder.get()
-                .withChart(ChartBuilder.get()
-                        .withType(Type.area)
-                        .withZoom(ZoomBuilder.get()
-                                .withEnabled(false)
+                .withChart(ChartBuilder.get().withType(Type.donut).build())
+                .withLegend(LegendBuilder.get()
+                        .withPosition(Position.right)
+                        .build())
+                .withSeries(44.0, 55.0, 41.0, 17.0, 15.0)
+//                .withSeries(series.toArray())
+                .withResponsive(ResponsiveBuilder.get()
+                        .withBreakpoint(480.0)
+                        .withOptions(OptionsBuilder.get()
+                                .withLegend(LegendBuilder.get()
+                                        .withPosition(Position.bottom)
+                                        .build())
                                 .build())
                         .build())
-                .withDataLabels(DataLabelsBuilder.get()
-                        .withEnabled(false)
-                        .build())
-                .withStroke(StrokeBuilder.get().withCurve(Curve.straight).build())
-                .withSeries(new Series<>("STOCK ABC", 10.0, 41.0, 35.0, 51.0, 49.0, 62.0, 69.0, 91.0, 148.0))
-                .withTitle(TitleSubtitleBuilder.get()
-                        .withText("Fundamental Analysis of Stocks")
-                        .withAlign(Align.left).build())
-                .withSubtitle(TitleSubtitleBuilder.get()
-                        .withText("Price Movements")
-                        .withAlign(Align.left).build())
-                .withLabels(IntStream.range(1, 10).boxed().map(day -> LocalDate.of(2000, 1, day).toString()).toArray(String[]::new))
-                .withXaxis(XAxisBuilder.get()
-                        .withType(XAxisType.datetime).build())
-                .withYaxis(YAxisBuilder.get()
-                        .withOpposite(true).build())
-                .withLegend(LegendBuilder.get().withHorizontalAlign(HorizontalAlign.left).build())
                 .build();
     }
 }
